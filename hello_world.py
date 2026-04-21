c54e72e57fcee40069e14ef4cc1cf9a99d01c1fb
Desh <153073729+deshen-naidoo@users.noreply.github.com>
2026-04-21 23:03:43 +0200

    M03T09 – Version Control and Git Basics task submission

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+"""
+A simple script to demonstrate Git version control.
+"""
+
+def print_hello():
+    """
+    Prints a greeting message to the console.
+    """
+    message = "Hello, this is my example repository!"
+    print(message)
+
+if __name__ == "__main__":
+    print_hello()
